170f2d3d4c61323137e73becdbd6869c7c801472
AHMED\ahmed <ahmed01142043116@gmail.com>
2025-12-31 00:36:06 +0200

    fix: use SupabaseService instead of SupabaseClient in DI

--- lib/core/di/injections/merchant_injection.dart
+++ lib/core/di/injections/merchant_injection.dart
@@ -1,12 +1,12 @@
 import 'package:get_it/get_it.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import '../../services/supabase_service.dart';
 import '../../../features/merchant/data/datasources/inventory_remote_datasource.dart';
 import '../../../features/merchant/presentation/cubit/inventory_insights_cubit.dart';
 
 void registerMerchantDependencies(GetIt sl) {
   // Data Sources
   sl.registerLazySingleton<InventoryRemoteDataSource>(
-    () => InventoryRemoteDataSourceImpl(sl<SupabaseClient>()),
+    () => InventoryRemoteDataSourceImpl(sl<SupabaseService>().client),
   );
 
   // Cubits
